58a5a66b0eb915d8f53320a4b5f071936562ff5f
abid1174 <abidalamin7@gmail.com>
2021-12-15 14:08:53 +0600

    first commit

---
+++ dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+function foo(bar) {
+    return 'Hello, ' + bar;
+}
+let baz = 'ABC';
+console.log(foo(baz));
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+function foo(bar: string) {
+    return 'Hello, ' + bar
+}
+
+let baz = 'ABC'
+
+console.log(foo(baz))
